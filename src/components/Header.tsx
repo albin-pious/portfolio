91a14dee8339dc2510ea088bc43ee91ea63f2f00
Albin Pious <albinpious65@gmail.com>
2024-06-10 20:46:46 +0530

    initail commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC<{}> = ()=>{
+    return(
+        <div className="backdrop-blur-3xl">
+            <p className="text-center">Header</p>
+        </div>
+    )
+}
+
+export default Header;
